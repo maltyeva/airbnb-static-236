4c968d7fe984e84ff42478aaf8561b493d338bf6
Maria <maria.altyeva@gmail.com>
2019-03-25 18:05:25 +0800

    added flat imae

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,19 +1,23 @@
 require "open-uri"
 
 class FlatsController < ApplicationController
-  def index
-    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
-    @flats = JSON.parse(open(url).read)
+  before_action :set_flats, only: [:index, :show]
 
+  def index
   end
 
   def show
     @id = params[:id].to_i
+
+    @flat = @flats.select{ |flat| flat["id"] == @id }.first
+  end
+
+  def new
+
+  end
+
+  def set_flats
     url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
     @flats = JSON.parse(open(url).read)
-    @flat = @flats.select{ |flat| flat["id"] == @id }
-
-
-
   end
 end
